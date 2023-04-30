0d3e7ea71985ddf0812a1114d7b604ef0794b949
snowfallen <bondarenkoov1998@gmail.com>
2023-04-30 10:58:46 +0200

    add number and srting arrays  type/structure variables simlpe examples

--- index.ts
+++ index.ts
@@ -8,3 +8,10 @@ console.log(int);
 
 const float: number = 5.14;
 console.log(float);
+
+//array data structure ?? type ??
+const arrayNumber: number[] = [1,1,2,3,5,8,13];
+const arrayNumber2: Array<number> = [1,1,2,3,5,8,13];
+
+const arrayString: string[] = ['q','w','e','r','t','y','1'];
+const arrayString2: Array<string> = ['q','w','e','r','t','y','2'];
